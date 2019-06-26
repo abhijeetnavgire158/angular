1768de1698680d70385b9e968bb1c8782819f7e4
Abhijeet Navgire <abhijeet.navgire@cuelogic.com>
2019-06-26 19:18:00 +0530

    Property Binding & Event Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverId: Number = 10;
+    serverStatus: String = 'Offline';
+    allowedNewServer: Boolean = false;
+    newServerMessage: String = 'No new server is created.';
+    serverName: String = '';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
+    constructor() {
+        setTimeout(() => {
+            this.allowedNewServer = true;
+        }, 2000);
+    }
+
+    onCreateServer() {
+        this.newServerMessage = 'New server was created! With name ' + this.serverName;
+    }
+
+    onUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 }
